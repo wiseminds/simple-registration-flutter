14bb1a256da79be7c1ed50aed8d49fcf60711bbb
Wisdom Ekeh <ekeh.wisdom@gmail.com>
2021-10-26 02:52:16 +0100

    added fonts and colors constants file

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xff1B3F28);
+  static const borderColor = Color(0xff8D8C8C);
+
+  static const textBlack = Color(0xff323232);
+  static const backgroundColorLight = Color(0xffF2F2F2);
+  static const backgroundColorDark = Color(0xFF110404);
+
+  static const cardColorLight = Color(0xFFFCFCFC);
+  static const cardColorDark = Color(0xFF180D0D);
+
+  static const canvasColorDark = Color(0xFF1A0D0D);
+}
