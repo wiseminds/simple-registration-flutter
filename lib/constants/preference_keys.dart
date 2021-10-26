2cb1cc52be89c83924f54dea83905278c44e6700
Wisdom Ekeh <ekeh.wisdom@gmail.com>
2021-10-26 03:28:48 +0100

    added preference keys constants fie

---
+++ lib/constants/preference_keys.dart
@@ -0,0 +1,5 @@
+/// a class storing keys used in preference library
+class PreferenceKeys {
+  /// key  to be used as file name for the hive database
+  static const localStorage = 'local-storage';
+}
