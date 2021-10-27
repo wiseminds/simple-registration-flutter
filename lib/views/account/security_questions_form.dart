6df4f93a2a126ab050f7d3cbf809ee02cb5aed15
Wisdom Ekeh <ekeh.wisdom@gmail.com>
2021-10-27 14:33:11 +0100

    finished security questions form

---
+++ lib/views/account/security_questions_form.dart
@@ -0,0 +1,206 @@
+import 'package:challenge/constants/dimens.dart';
+import 'package:challenge/models/security_question.dart';
+import 'package:challenge/widgets/dropdown_input.dart';
+import 'package:challenge/widgets/medium_button.dart';
+import 'package:challenge/widgets/spring_progressbar.dart';
+import 'package:flutter/material.dart';
+import 'package:challenge/core/extensions/index.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'auth_header.dart';
+import 'bloc/app_form_bloc.dart';
+import 'bloc/auth_bloc.dart';
+
+class SecurityQuestionsForm extends StatefulWidget {
+  final AppFormBloc formBloc;
+  final Function() onSuccess;
+  const SecurityQuestionsForm(
+      {Key? key, required this.onSuccess, required this.formBloc})
+      : super(key: key);
+
+  @override
+  _SecurityQuestionsFormState createState() => _SecurityQuestionsFormState();
+}
+
+class _SecurityQuestionsFormState extends State<SecurityQuestionsForm> {
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  final TextEditingController _qController1 = TextEditingController();
+  final TextEditingController _qController2 = TextEditingController();
+
+  @override
+  void initState() {
+    _qController1.text = widget.formBloc.state.answer1 ?? '';
+    _qController2.text = widget.formBloc.state.answer2 ?? '';
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        color: context.backgroundColor,
+        child: SafeArea(
+          child: BlocBuilder<AuthBloc, AuthState>(
+            builder: (context, authState) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: Dimens.paddingNormal),
+                child: AutofillGroup(
+                  child: Form(
+                    autovalidateMode: autovalidateMode,
+                    child: ListView(
+                      children: [
+                        20.0.h,
+                        const SpringProgressbar(progress: 4 / 4),
+                        20.0.h,
+                        const AuthHeader(
+                          title: 'Recovery options',
+                          subtitle: 'Pease set up security questions',
+                        ),
+                        40.0.h,
+                        BlocBuilder<AppFormBloc, AppFormState>(
+                            bloc: widget.formBloc,
+                            builder: (context, state) {
+                              return DropdownInput<SecurityQuestion>(
+                                enabled: authState is! LoadingAuthState,
+                                label: 'Security Question 1',
+                                hint: 'Security Question',
+                                showToplabel: state.question != null,
+                                value: state.question,
+                                errorText: state.question != null
+                                    ? null
+                                    : 'please select security questions',
+                                options: [
+                                  for (var item in securityQuestions)
+                                    InputItem(item, item.question)
+                                ],
+                                onChanged: (value) {
+                                  if (value != null &&
+                                      state.altQuestion != value) {
+                                    widget.formBloc
+                                        .add(SecurityQuestionChanged(value));
+                                  }
+                                },
+                              );
+                            }),
+                        40.0.h,
+                        BlocBuilder<AppFormBloc, AppFormState>(
+                          bloc: widget.formBloc,
+                          buildWhen: (prev, current) =>
+                              prev.answer1 != current.answer1,
+                          builder: (context, state) {
+                            return TextFormField(
+                              enabled: authState is! LoadingAuthState,
+                              controller: _qController1,
+                              // focusNode: _focusNode,
+                              autovalidateMode: autovalidateMode,
+                              style: context.bodyText1,
+                              validator: (text) => (text ?? '').isEmpty
+                                  ? 'Please enter security answer'
+                                  : null,
+                              onChanged: (value) =>
+                                  widget.formBloc.add(Answer1Changed(value)),
+                              //added 3 digit to compensate for space added by formatter
+                              maxLength: 100,
+                              decoration: InputDecoration(
+                                  counterStyle: context.caption!
+                                      .copyWith(fontSize: .0001, height: .001),
+                                  labelText: 'Security answer 1',
+                                  hintText: 'Please enter security answer'),
+                            );
+                          },
+                        ),
+                        30.0.h,
+                        BlocBuilder<AppFormBloc, AppFormState>(
+                          bloc: widget.formBloc,
+                          builder: (context, state) {
+                            return DropdownInput<SecurityQuestion>(
+                              enabled: authState is! LoadingAuthState,
+                              label: 'Security Question 2',
+                              hint: 'Security Question',
+                              showToplabel: state.altQuestion != null,
+                              value: state.altQuestion,
+                              errorText: state.altQuestion != null
+                                  ? null
+                                  : 'please select security questions',
+                              options: [
+                                for (var item in securityQuestions)
+                                  InputItem(item, item.question)
+                              ],
+                              onChanged: (value) {
+                                if (value != null && state.question != value) {
+                                  widget.formBloc
+                                      .add(AltSecurityQuestionChanged(value));
+                                }
+                              },
+                            );
+                          },
+                        ),
+                        30.0.h,
+                        BlocBuilder<AppFormBloc, AppFormState>(
+                          bloc: widget.formBloc,
+                          buildWhen: (prev, current) =>
+                              prev.answer2 != current.answer2,
+                          builder: (context, state) {
+                            return TextFormField(
+                              enabled: authState is! LoadingAuthState,
+                              controller: _qController2,
+                              // focusNode: _focusNode,
+                              autovalidateMode: autovalidateMode,
+                              style: context.bodyText1,
+                              keyboardType: TextInputType.name,
+                              textCapitalization: TextCapitalization.words,
+                              validator: (text) => (text ?? '').isEmpty
+                                  ? 'Please enter security answer'
+                                  : null,
+                              onChanged: (value) =>
+                                  widget.formBloc.add(Answer2Changed(value)),
+                              //added 3 digit to compensate for space added by formatter
+                              maxLength: 50,
+                              decoration: InputDecoration(
+                                  counterStyle: context.caption!
+                                      .copyWith(fontSize: .0001, height: .001),
+                                  labelText: 'Security answer 2',
+                                  hintText: 'Please enter Security answer '),
+                            );
+                          },
+                        ),
+                        100.0.h,
+                        BlocBuilder<AppFormBloc, AppFormState>(
+                            bloc: widget.formBloc,
+                            builder: (context, state) => MediumButton(
+                                  label: 'Submit',
+                                  isLoading: authState is LoadingAuthState,
+                                  onPressed: () {
+                                    if (autovalidateMode ==
+                                        AutovalidateMode.disabled) {
+                                      setState(() {
+                                        autovalidateMode =
+                                            AutovalidateMode.always;
+                                      });
+                                    }
+                                    if (state.isSecurityFormValid) {
+                                      context
+                                          .read<AuthBloc>()
+                                          .add(SubmitRegister(state.formToMap));
+                                      widget.onSuccess();
+                                    } else {
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(const SnackBar(
+                                        content:
+                                            Text('Please fill in all fields'),
+                                        backgroundColor: Colors.red,
+                                      ));
+                                    }
+                                  },
+                                )),
+                        50.0.h,
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ));
+  }
+}
