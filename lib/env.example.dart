119a6b91e10de2bbf94fac7441bfb6a7b890b859
Wisdom Ekeh <ekeh.wisdom@gmail.com>
2021-10-27 14:09:44 +0100

    updated env

--- lib/env.example.dart
+++ lib/env.example.dart
@@ -1,5 +1,4 @@
-const ENV = {
-  'LOCATION_BASE_URL': 'http://locationsng-api.herokuapp.com',
-  'PRODUCTION_BASE_URL': 'https://google.com',
-  'DEVELOPMENT_BASE_URL': 'localhost:3000'
-};
+// const ENV = {
+//   'PRODUCTION_BASE_URL': 'https://www.mybackup.com.ng',
+//   'DEVELOPMENT_BASE_URL': 'localhost:3000'
+// };
